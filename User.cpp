f5bde3274ca7ad557e77e4a4858655b2bd149ac9
menna2001 <76685404+menna2001@users.noreply.github.com>
2021-05-30 14:11:12 +0200

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -1,1 +1,38 @@
+#include "User.h"
+#include<iostream>
+
+using namespace std;
+
+User::User(){
+
+
+
+
+}
+
+void User::Register() {
+
+}
+
+void User::Login() {
+
+
+}
+
+void User::Update() {
+
+
+
+}
+void User::Delete() {
+
+
+
+}
+
+User::~User() {
+
+
+}
+
 

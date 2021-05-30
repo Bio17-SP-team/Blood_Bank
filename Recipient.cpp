ccbae6ee48fecaf3cee0a54998dfa6d4746dede4
jana-source <63115028+jana-source@users.noreply.github.com>
2021-05-30 15:45:05 +0200

    Create Recipient.cpp

---
+++ Recipient.cpp
@@ -0,0 +1,87 @@
+#include "Recipient .h"+
+ #include<iostream>
+ #include"stdio.h"
+ using namespace std ;
+
+Recipient ::Recipient ()
+{
+}
+ bool Recipient ::searchForAvalabilityOfBloodTypes()
+{  
+     Blood b ; // class Blood 
+     User u ;
+     
+     int quantity;
+     u.bloodType ;
+     if (u.bloodType == AB+ ) 
+     {
+         
+         
+     }
+     else  if (u.bloodType ==AB- ) 
+     {
+         
+         
+     }
+    else  if (u.bloodType ==A+ ) 
+     {
+         
+         
+     }
+      else if (u.bloodType == A-) 
+     {
+         
+         
+     }
+    else  if (u.bloodType == B+) 
+     {
+         
+         
+         
+     }
+    else if (u.bloodType ==B- ) 
+     {
+         
+         
+     }
+    else  if (u.bloodType ==O- ) 
+     {
+         
+         
+     }
+     else  if (u.bloodType == O+) 
+     {
+         
+         
+     }
+     
+
+ cout<<" Enter the blood quantity you want to take "<<endl<<endl;
+ cin>>  quantity;    
+ 
+ 
+}
+
+
+ void Recipient ::DisaplayBloodTypes()
+{
+
+
+
+    //ctor
+}
+ void Recipient ::         RequestABloodType()
+{
+    //ctor
+}
+
+
+
+
+
+
+
+Recipient ::~Recipient ()
+{
+    //dtor
+}

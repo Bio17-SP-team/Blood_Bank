f14aa79138dd26a2a0ff8fc4fa1a1c262bfeb169
jana-source <63115028+jana-source@users.noreply.github.com>
2021-05-30 15:46:00 +0200

    Create Recipient.h

---
+++ Recipient.h
@@ -0,0 +1,30 @@
+#ifndef RECIPIENT _H
+#define RECIPIENT _H
+
+
+class Recipient
+{
+    public:
+        Recipient ();
+
+       // login();
+        //Register();
+       // update_data ();
+        // delte_account ();
+       //  home_page();
+
+
+
+
+
+         bool searchForAvalabilityOfBloodTypes(); //request  ( calling )
+         void DisaplayBloodTypes();// ele ana 25atooo // ask for functionality  /*alert */
+          void RequestABloodType();// blood type  , quantity ,( hospital ( from his data )
+
+
+        virtual ~Recipient ();
+    protected:
+    private:
+};
+
+#endif // RECIPIENT _H

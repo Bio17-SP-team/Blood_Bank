4187bb4041926ef7a76f68ffb4b3656a33e232a6
habiba269 <62194228+habiba269@users.noreply.github.com>
2021-05-30 14:37:53 +0200

    Update Blood.h

--- Blood.h
+++ Blood.h
@@ -1,1 +1,15 @@
 
+#pragma once
+#include<iostream>
+#include<string>
+using namespace std;
+class Blood
+{
+	string blood_type;
+	string recieved_data;
+	int quantity;
+	Blood(string, string, int);
+	~Blood();
+
+};
+

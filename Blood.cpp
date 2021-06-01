168853d4bd58baa05ac4ceaffdfa63398a03fead
habiba269 <62194228+habiba269@users.noreply.github.com>
2021-06-01 10:11:47 +0200

    Update Blood.cpp

--- Blood.cpp
+++ Blood.cpp
@@ -2,3 +2,18 @@
 #include "Blood.h"
 Blood::Blood(string b, string r, int q) :blood_type(b), recieved_data(r), quantity(q) {};
 
+
+
+
+************************
+//bngarab
+#include "Blood.h"
+#include"Blood_Bank.h"
+#include<assert.h>
+#include<queue>
+#include"Blood_Bank.cpp"
+#include<iostream>
+using namespace std;
+Blood::Blood(string b, time_t expiry_date, int q) :blood_type(b), tmNow(expiry_date), quantity(q) {};
+
+

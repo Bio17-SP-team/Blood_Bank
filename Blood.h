e32e9dedfacae9305ea8c8aec583e81d51c5512d
habiba269 <62194228+habiba269@users.noreply.github.com>
2021-06-01 10:10:48 +0200

    Update Blood.h

--- Blood.h
+++ Blood.h
@@ -13,3 +13,22 @@ class Blood
 
 };
 
+*****************
+//bngarab hena
+#pragma once
+#include<iostream>
+#include<string>
+#include<ctime>
+using namespace std;
+class Blood
+{
+	string blood_type;
+	int quantity;
+	time_t tmNow = time(0);
+	char* dt = ctime(&tmNow);
+	Blood(string ,time_t , int);
+	Blood();
+	~Blood();
+
+};
+

8fdcb7f7b90285265f96cbab760ce6c880777dd0
manarelbagoury <62239019+manarelbagoury@users.noreply.github.com>
2021-06-05 03:04:09 +0200

    Update Donor.h

--- Donor.h
+++ Donor.h
@@ -1,1 +1,18 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "user.cpp"
 
+using namespace std;
+class Donor : public User
+{
+public: 
+int age;
+bool illnesses;
+string others;
+string date;
+Donor();
+void Register();
+void Update();
+bool donationReq();
+};

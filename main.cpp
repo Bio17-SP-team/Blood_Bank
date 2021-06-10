5ff3798e0ab990f83301b60e302ef4b05052c594
menna2001 <76685404+menna2001@users.noreply.github.com>
2021-06-10 18:33:13 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,22 +2,30 @@
 #include <string>
 #include <stdio.h>
 #include <queue>
-#include "blood_bank.cpp"
+#include <vector>
+#include "blood_bank.h"
+#include "donor.h"
 #include "read_write_files.cpp"
-using	namespace	std;
+#include "recipient.h"
+using namespace	std;
 
 int	main()
 {
-    Blood_Bank b;
-	read_write_files rw;
-    rw.read_file(b.Aplus, "Aplus.txt");
-    rw.read_file(b.Aminus, "Aminus.txt");
-    b.Aplus.push("test");
-    b.Aplus.push("t3");
-    b.Aplus.pop();
-    b.Aplus.push("mana4r");
-    b.Aminus.push("1");
-    b.Aminus.push("ll ii");
-    rw.write_file(b.Aplus, "Aplus.txt");
-    rw.write_file(b.Aminus, "Aminus.txt");
+    Blood_Bank bank;
+    read_write_files rw;
+    vector<Recipient> rlist;
+    rw.read_recipient(rlist, "recipients.txt");
+    rw.read_queue(bank.A, "A.txt");
+    rw.read_queue(bank.B, "B.txt");
+    rw.read_queue(bank.O, "O.txt");
+    rw.read_queue(bank.AB, "AB.txt");
+    Recipient u;
+    u.Register();
+    u.DisplayBloodTypes(bank);
+    rlist.push_back(u);
+    rw.write_recipient(rlist, "recipients.txt");
+    rw.write_queue(bank.A, "A.txt");
+    rw.write_queue(bank.B, "B.txt");
+    rw.write_queue(bank.O, "O.txt");
+    rw.write_queue(bank.AB, "AB.txt");
 }

a9e58d79be34afef46cdb0e5a219a63039a253e0
manarelbagoury <62239019+manarelbagoury@users.noreply.github.com>
2021-06-14 00:25:08 +0200

    Update Donor.h

--- Donor.h
+++ Donor.h
@@ -1,18 +1,27 @@
 #pragma once
 #include <iostream>
 #include <string>
-#include "user.cpp"
+#include <vector>
+#include "User.h"
 
 using namespace std;
+
+struct date {
+	int day, month, year = 0;
+};
+
 class Donor : public User
 {
-public: 
-int age;
-bool illnesses;
-string others;
-string date;
-Donor();
-void Register();
-void Update();
-bool donationReq();
+public:
+	int age;
+	bool illnesses;
+	string others;
+	int date;
+	Donor();
+	void Register(vector<Donor>&);
+	static void Login(vector<Donor>&);
+	void Update();
+	void Delete();
+	void HomePage();
+	bool DonationReq();
 };

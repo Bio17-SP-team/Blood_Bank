96bcda7d278de45757c8da97a61edfc6ac2d1a5f
manarelbagoury <62239019+manarelbagoury@users.noreply.github.com>
2021-06-14 00:24:24 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,29 +3,79 @@
 #include <stdio.h>
 #include <queue>
 #include <vector>
-#include "blood_bank.h"
-#include "donor.h"
+#include "Blood_Bank.h"
+#include "Donor.h"
 #include "read_write_files.cpp"
-#include "recipient.h"
+#include "Recipient.h"
 using namespace	std;
+
+void mainmenu(vector<Donor> d, vector<Recipient> r, Blood_Bank b) {
+	while (true) {
+		cout << "\t\t\tWelcome to Blood Bank System\n";
+		cout << "To enter as a donor, press 1\nTo enter as a recipient press 2\nIf you want to exit, press any other key: ";
+		int mainchoice, subchoice;
+		cin >> mainchoice;
+		if (mainchoice == 1) {
+			cout << "To register, press 1\nTo login, press 2: ";
+			cin >> subchoice;
+			while (subchoice < 1 || subchoice > 2)
+			{
+				cout << "Invalid Input!" << endl;
+				cout << "Try Again" << endl;
+				cin >> subchoice;
+			}
+
+			if (subchoice == 1) {
+				Donor donor;
+				donor.Register(d);
+				d.push_back(donor);
+				Donor::Login(d);
+			}
+			else if (subchoice == 2) {
+				Donor::Login(d);
+			}
+
+		}
+		else if(mainchoice == 2) {
+			cout << "To register, press 1\nTo login, press 2: ";
+			cin >> subchoice;
+			while (subchoice < 1 || subchoice > 2)
+			{
+				cout << "Invalid Input!" << endl;
+				cout << "Try Again" << endl;
+				cin >> subchoice;
+			}
+
+			if (subchoice == 1) {
+				Recipient recipient;
+				recipient.Register(r, b);
+				r.push_back(recipient);
+				Recipient::Login(r, b);
+			}
+			else if (subchoice == 2) {
+				Recipient::Login(r, b);
+			}
+		}
+		else {
+			break;
+		}
+	}
+}
 
 int	main()
 {
-    Blood_Bank bank;
-    read_write_files rw;
-    vector<Recipient> rlist;
-    rw.read_recipient(rlist, "recipients.txt");
-    rw.read_queue(bank.A, "A.txt");
-    rw.read_queue(bank.B, "B.txt");
-    rw.read_queue(bank.O, "O.txt");
-    rw.read_queue(bank.AB, "AB.txt");
-    Recipient u;
-    u.Register();
-    u.DisplayBloodTypes(bank);
-    rlist.push_back(u);
-    rw.write_recipient(rlist, "recipients.txt");
-    rw.write_queue(bank.A, "A.txt");
-    rw.write_queue(bank.B, "B.txt");
-    rw.write_queue(bank.O, "O.txt");
-    rw.write_queue(bank.AB, "AB.txt");
+	read_write_files rw;
+	vector<Donor> donors;
+	vector<Recipient> recipients;
+	Blood_Bank bloodbank;
+	rw.read_queue(bloodbank.A, "A.txt");
+	rw.read_queue(bloodbank.B, "B.txt");
+	rw.read_queue(bloodbank.AB, "AB.txt");
+	rw.read_queue(bloodbank.O, "O.txt");
+	mainmenu(donors, recipients, bloodbank);
+	rw.write_queue(bloodbank.A, "A.txt");
+	rw.write_queue(bloodbank.B, "B.txt");
+	rw.write_queue(bloodbank.AB, "AB.txt");
+	rw.write_queue(bloodbank.O, "O.txt");
+
 }

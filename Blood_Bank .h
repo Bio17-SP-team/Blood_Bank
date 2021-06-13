2e1c5ac6e5fbc8bff33b68d99d7221bf3fe03e74
manarelbagoury <62239019+manarelbagoury@users.noreply.github.com>
2021-06-14 00:28:06 +0200

    Update Blood_Bank .h

--- Blood_Bank .h
+++ Blood_Bank .h
@@ -1,25 +1,20 @@
 #pragma once
-#include<deque>
-#include"Blood.cpp"
+#include<queue>
 #include<string>
+
 using namespace std;
+
 class Blood_Bank
 {
 public:
-	deque<Blood> Aplus;
-	queue<Blood> Aminus;
-	queue<Blood> Bplus;
-	queue<Blood> Bminus;
-	queue<Blood> Oplus;
-	queue<Blood> Ominus;
-	queue<Blood> ABplus;
-	queue<Blood> ABminus;
-	bool searchForAvalabilityOfBloodTypes(int, string);
-	void Request(bool);
-	void Waiting_list(bool);
+	queue<int> requestlist;
+	queue<int> waitinglist;
+	queue<int> donationlist;
+	queue<string> A;
+	queue<string> B;
+	queue<string> O;
+	queue<string> AB;
 	Blood_Bank();
-	~Blood_Bank();
 
 
 };
-

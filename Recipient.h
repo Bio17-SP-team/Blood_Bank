0d73d1197f154c06e374cb0b5842f57ac6eac826
manarelbagoury <62239019+manarelbagoury@users.noreply.github.com>
2021-12-16 08:56:22 +0200

    Update Recipient.h

--- Recipient.h
+++ Recipient.h
@@ -5,21 +5,25 @@
 #include <vector>
 #include <string>
 #include "User.h"
+#include "Date_time.h"
 
 using namespace std;
 class Recipient : public User
 {
+private:
+	Date_time date_time;
 public:
-	int age;
 	string hospital;
 	string case_doctor;
 	Recipient();
-	void Register(vector<Recipient>&, Blood_Bank&);
+	void Register();
 	static void Login(vector<Recipient>&, Blood_Bank&);
 	void Update();
-	void Delete();
+	static void Delete(vector<Recipient>&);
 	void HomePage(Blood_Bank&);
 	bool searchForAvailabilityOfBloodTypes(Blood_Bank&);
 	void DisplayBloodTypes(Blood_Bank&);// ele ana 25atooo // ask for functionality  /*alert */
 	void RequestABloodType(Blood_Bank&);// blood type  , quantity ,( hospital ( from his data )
+	void setHospital();
+	void setDoctor();
 };
